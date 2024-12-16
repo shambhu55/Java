2c3ca445214ce5bc6c3bacd67be4e2e08e093a06
Shambhuling Konapure <139031921+shambhu55@users.noreply.github.com>
2024-12-16 16:59:37 +0530

    Add files via upload

---
+++ Java Programs/DoWhile.java
@@ -0,0 +1,15 @@
+// DO WHILE LOOP or EXIT CONTRL LOOP
+
+class DoWhile
+{
+	public static void main(String args[])
+	{
+		int a = 1;
+		do
+		{
+			System.out.println(a);
+			a++;
+		}while(a<=10);
+	}
+
+}
