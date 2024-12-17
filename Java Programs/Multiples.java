7b2b4d0a3875445121597c5c137eee3b964ddfde
Shambhuling Konapure <139031921+shambhu55@users.noreply.github.com>
2024-12-17 21:33:17 +0530

    Add files via upload

---
+++ Java Programs/Multiples.java
@@ -0,0 +1,30 @@
+/*
+
+Print multiples of 10, as follow
+
+10 x 1 = 10
+10 x 2 = 20
+10 x 3 = 30
+10 x 4 = 40
+10 x 5 = 50
+10 x 6 = 60
+10 x 7 = 70
+10 x 8 = 80
+10 x 9 = 90
+10 x 10 = 100
+
+*/
+
+
+
+class Multiples
+{
+	public static void main(String args[])
+	{
+		for(int i=1; i<=10; i++)
+		{
+			System.out.println("10 x "+i +" = " +(10*i));
+		}
+
+	}
+}
