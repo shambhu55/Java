0f7ea038f2a4027cb6554538b13c6592d1f2a63d
Shambhuling Konapure <139031921+shambhu55@users.noreply.github.com>
2024-12-17 21:32:18 +0530

    Add files via upload

---
+++ Java Programs/RectPattern.java
@@ -0,0 +1,38 @@
+/*
+
+Print 5*5 rectangular pattern.
+
+*****
+*****
+*****
+*****
+*****
+ 
+
+*/
+
+
+class RectPattern
+{
+	public static void main(String args[])
+	{
+		
+		for(int i = 1; i<=5; i++)
+		{
+			for(int j=1; j<=5; j++)
+			{
+			  System.out.print("*");
+			}
+
+			System.out.println();
+		}
+
+	}
+}
+
+
+
+
+
+
+
