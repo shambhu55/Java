c70b6e78ffe895d02287aae2551b4b54fc35b81b
Shambhuling Konapure <139031921+shambhu55@users.noreply.github.com>
2024-12-30 17:19:35 +0530

    Add files via upload

---
+++ Java Programs/Triangle Left Side/TriPatternCounterAlpha7.java
@@ -0,0 +1,35 @@
+/*
+
+KGDBA
+LHEC
+MIF
+NJ
+O
+
+11	7	4	2	1
+12	8	5	3
+13	9	6
+14	10
+15
+
+*/
+
+class TriPatternCounterAlpha7
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		int count=((n*(n-1))/2)+1;
+		for(int i=1; i<=n; i++)
+		{
+			int c=count;
+			for(int j=n; j>=i; j--)
+			{
+				System.out.print((char)(c+64));
+				c-=j-1;
+			}
+			System.out.println();
+			count++;
+		}
+	}
+}
