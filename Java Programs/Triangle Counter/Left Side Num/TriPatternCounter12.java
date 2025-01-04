0b188816cae3a36b0b41fefa096ba02ce58567be
Shambhuling Konapure <139031921+shambhu55@users.noreply.github.com>
2025-01-04 17:14:08 +0530

    Add files via upload

---
+++ Java Programs/Triangle Counter/Left Side Num/TriPatternCounter12.java
@@ -0,0 +1,30 @@
+/*
+
+11
+7	12
+4	8	13
+2	5	9	14
+1	3	6	10	15	
+
+*/
+
+class TriPatternCounter12
+{
+	public static void main(String args[])
+	{
+		int n=5;		
+		int count=(n*(n-1)/2)+1;
+
+		for(int i=n; i>=1; i--)
+		{
+			int c=count;
+			for(int j=i; j<=n; j++)
+			{
+				System.out.print(c+"\t");
+				c+=j+1;
+			}
+			System.out.println();
+			count-=i-1;
+		}
+	}
+}
