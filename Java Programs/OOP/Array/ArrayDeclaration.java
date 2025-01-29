b687476221dc2c6499c2fc736fd626158d5710ba
Shambhuling Konapure <139031921+shambhu55@users.noreply.github.com>
2025-01-29 14:41:58 +0530

    Add files via upload

---
+++ Java Programs/OOP/Array/ArrayDeclaration.java
@@ -0,0 +1,15 @@
+class ArrayDeclaration
+{
+	public static void main(String args[])
+	{
+		int a[]=new int[5];
+		int [0]=10;
+		int [1]=13;
+		int [2]=15;
+		int [3]=21;
+		int [4]=35;
+
+		for(int i=0; i<a.length; i++)
+			System.out.println(a[i]);
+	}
+}
