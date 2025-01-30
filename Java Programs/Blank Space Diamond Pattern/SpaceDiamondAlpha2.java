db2ac2962c49e1305de3b586487d911897fb8e68
Shambhuling Konapure <139031921+shambhu55@users.noreply.github.com>
2025-01-30 17:29:50 +0530

    Add files via upload

---
+++ Java Programs/Blank Space Diamond Pattern/SpaceDiamondAlpha2.java
@@ -0,0 +1,65 @@
+/*
+
+A       A
+Ab     bA
+AbC   CbA
+AbCd dCbA
+AbCdEdCbA
+AbCd dCbA
+AbC   CbA
+Ab     bA
+A       A
+
+*/
+
+class SpaceDiamondAlpha2
+{
+	public static void main(String args[])
+	{
+		int n=5;
+
+		for(int i=1; i<=n; i++)
+		{
+			if(i==n)
+			{
+				for(int j=1; j<=n; j++)
+					System.out.print((j%2!=0)?(char)(j+64):(char)(j+96));
+				for(int j=n-1; j>=1; j--)
+					System.out.print((j%2!=0)?(char)(j+64):(char)(j+96));
+			}
+
+			else
+			{
+				for(int j=1; j<=i; j++)
+					System.out.print((j%2!=0)?(char)(j+64):(char)(j+96));
+				for(int j=1; j<=((n-i)*2)-1; j++)
+					System.out.print(" ");
+				for(int j=i; j>=1; j--)
+					System.out.print((j%2!=0)?(char)(j+64):(char)(j+96));
+			}
+			System.out.println();
+		}
+
+		for(int i=n-1; i>=1; i--)
+		{
+			if(i==n)
+			{
+				for(int j=1; j<=n; j++)
+					System.out.print((j%2!=0)?(char)(j+64):(char)(j+96));
+				for(int j=n-1; j>=1; j--)
+					System.out.print((j%2!=0)?(char)(j+64):(char)(j+96));
+			}
+
+			else
+			{
+				for(int j=1; j<=i; j++)
+					System.out.print((j%2!=0)?(char)(j+64):(char)(j+96));
+				for(int j=1; j<=((n-i)*2)-1; j++)
+					System.out.print(" ");
+				for(int j=i; j>=1; j--)
+					System.out.print((j%2!=0)?(char)(j+64):(char)(j+96));
+			}
+			System.out.println();
+		}
+	}
+}
