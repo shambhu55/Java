8f299d586d9fb4e7c186451d409c10ce09969a40
Shambhuling Konapure <139031921+shambhu55@users.noreply.github.com>
2025-02-03 17:31:53 +0530

    Add files via upload

---
+++ Java Programs/Blank Space Diamond Pattern/HalfLowerDiamondAlpha4.java
@@ -0,0 +1,35 @@
+/*
+
+A       A
+bA     Ab
+CbA   AbC
+dCbA AbCd
+EdCbAbCdE
+
+*/
+
+class HalfLowerDiamondAlpha4
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		for(int i=1; i<=n; i++){
+			if(i==n){
+				for(int j=n; j>=1; j--)
+					System.out.print((j%2!=0)?(char)(j+64):(char)(j+96));
+				for(int j=2; j<=n; j++)
+					System.out.print((j%2!=0)?(char)(j+64):(char)(j+96));
+			}
+
+			else{
+				for(int j=i; j>=1; j--)
+					System.out.print((j%2!=0)?(char)(j+64):(char)(j+96));
+				for(int j=1; j<=((n-i)*2)-1; j++)
+					System.out.print(" ");
+				for(int j=1; j<=i; j++)
+					System.out.print((j%2!=0)?(char)(j+64):(char)(j+96));
+			}
+			System.out.println();
+		}
+	}
+}
