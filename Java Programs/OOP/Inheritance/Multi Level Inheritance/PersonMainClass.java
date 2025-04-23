79afba4b8ba9effa359db8d2092db1fe0df74a8c
Shambhuling Konapure <139031921+shambhu55@users.noreply.github.com>
2025-04-23 20:37:00 +0530

    Add files via upload

---
+++ Java Programs/OOP/Inheritance/Multi Level Inheritance/PersonMainClass.java
@@ -0,0 +1,10 @@
+class PersonMainClass
+{
+	public static void main(String args[])
+	{
+		PersonCity p = new PersonCity();
+		System.out.println(p.name("Mr. Ajay"));
+		System.out.println(p.id(12));
+		System.out.println(p.city("Pune"));
+	}
+}
