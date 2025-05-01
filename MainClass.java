c2007191479bef70b0b3db3129382c6e3c0f85b2
Shambhuling Konapure <139031921+shambhu55@users.noreply.github.com>
2025-05-01 18:35:22 +0530

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,9 @@
+class MainClass
+{
+	public static void main(String args[])
+	{
+		Demo2 d1=new Demo2();
+		d1.m1();
+	    //	d1.a=20; // we cannot assign a value to static & final variable a.
+	}
+}
