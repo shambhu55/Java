00aec19516ec3383877361f91789d8c0e8625a56
Shambhuling Konapure <139031921+shambhu55@users.noreply.github.com>
2025-05-03 18:48:30 +0530

    Delete Java Programs/Interface/Demo2.java

--- Java Programs/Interface/Demo2.java
+++
@@ -1,7 +0,0 @@
-class Demo2 implements Demo		// impliments(in interface) similar to extend.
-{
-	public void m1(){	// as we cant create obj., needs to override.
-		System.out.println("m1 method of Demo2");
-		System.out.println(a);
-	}
-}
