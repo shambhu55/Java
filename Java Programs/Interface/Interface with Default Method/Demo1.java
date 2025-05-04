5a7d5e0c4a5e71b5c25065c56ae1eccf2c2fe694
Shambhuling Konapure <139031921+shambhu55@users.noreply.github.com>
2025-05-04 23:31:19 +0530

    Add files via upload

---
+++ Java Programs/Interface/Interface with Default Method/Demo1.java
@@ -0,0 +1,12 @@
+interface Demo1
+{
+	default void m1()
+	{
+		System.out.println("m1 method of Demo1");
+	}
+
+	default void m3()
+	{
+		System.out.println("m3 method of Demo1");
+	}
+}
---
+++ Java Programs/Interface/Interface with Default Method/Demo2.java
@@ -0,0 +1,12 @@
+interface Demo2
+{
+	default void m2()
+	{
+		System.out.println("m2 method of Demo2");
+	}
+
+	default void m3()
+	{
+		System.out.println("m3 method of Demo2");
+	}
+}
