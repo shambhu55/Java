b5f543ee58fa829693531420c71fedded7142f14
Shambhuling Konapure <139031921+shambhu55@users.noreply.github.com>
2025-05-14 20:05:44 +0530

    Add files via upload

---
+++ Java Programs/Interface/Usage of Interface/Demo.java
@@ -0,0 +1,6 @@
+interface Demo	// have to use interface.
+{
+	int a=10, b=10;		// variables are final nd static
+	void m1();	// all methods are abstract and public by default.(so, 0 implement)
+			// and not required to use abstract keyword
+}
