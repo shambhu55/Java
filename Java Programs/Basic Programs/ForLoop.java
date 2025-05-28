63d6d54c818997d85fd7cbd879abbe577bee8026
Shambhuling Konapure <shambhukonapure55@gmail.com>
2025-05-28 23:41:49 +0530

    Update

---
+++ Java Programs/Basic Programs/ForLoop.java
@@ -0,0 +1,14 @@
+// FOR LOOP
+
+
+class ForLoop
+{
+	public static void main(String args[])
+	{
+		for(int a=1; a<=10; a++)
+		{
+			System.out.println(a);
+		}
+	}
+
+}
---
+++ Java Programs/Basic Programs/WhileLoop.java
@@ -0,0 +1,16 @@
+// WHILE LOOP or ENTRY CONTROL LOOP
+
+
+class WhileLoop
+{
+	public static void main(String args[])
+	{
+		int a = 1;
+		while(a<=10)
+		{
+			System.out.println(a);		//Instead of "System.out.println(a);" and "a++", this we can use "System.out.println(a++);"
+			a++;
+
+		}
+	}
+}
