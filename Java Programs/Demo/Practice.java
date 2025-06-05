71f3a4f7cbb27bb226ee7636241760cd55d9fd77
Shambhuling Konapure <shambhukonapure55@gmail.com>
2025-06-05 19:47:22 +0530

    Practice problem

--- Java Programs/Demo/Practice.java
+++ Java Programs/Demo/Practice.java
@@ -1,7 +1,37 @@
-int a, b;
+
 class Practice{
-    
-    void m1(){
-        System.out.println("a="+a+" b="+b);
+    int reverseNum(int a){
+        // Reverse Num
+        int sum=0;
+        while(a>0){
+            sum*=10;
+            sum=sum+(a%10);
+            a/=10;
+        }
+        return sum;
     }
+
+
+    // Method Overloading 
+        void m1(){
+            System.out.println("m1 called");
+        }
+
+        void m1(int a){
+            System.out.println("m2 called");
+        }
+
+
+        int add(int x, int y){
+            return x+y;
+        }
+        int add(int x, int y, int z){
+            return x+y+z;
+        }
+
+
+        // Default const : called during obj creation
+        Practice(){
+            System.out.println("Constructor called");
+        }
 }
--- Java Programs/Demo/PracticeMain.java
+++ Java Programs/Demo/PracticeMain.java
@@ -1,9 +1,18 @@
 public class PracticeMain {
     public static void main(String[] args) {
         Practice p = new Practice();
-        p.a=10;
-        p.b=20;
+
+        //reverse num
+        System.out.println("Reverse of 1234 : "+p.reverseNum(1234));
+
+        // Method overloading
         p.m1();
+        p.m1(10);
+
+        System.out.println("add 2 Num :"+p.add(10,20));
+        System.out.println("add 3 Num :"+p.add(10,20,30));
+
+        // constructor
+        
     }
 }
-
