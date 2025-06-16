b25eea1b5b6129ad6d8ef03d8c9ae6abc01e943d
Shambhuling Konapure <shambhukonapure55@gmail.com>
2025-06-16 22:42:21 +0530

    Packages

---
+++ Java Programs/Package/PackageDemo.java
@@ -0,0 +1,10 @@
+package a.b.c.d;
+public class PackageDemo
+{
+	public int a;
+	public void display()
+	{
+		System.out.println("Display method called");
+		System.out.println("a = "+a);
+	}
+}
---
+++ Java Programs/Package/PackageMainClass.java
@@ -0,0 +1,10 @@
+import a.b.c.d.PackageDemo;
+class PackageMainClass
+{
+	public static void main(String args[])
+	{
+		PackageDemo d1 = new PackageDemo();
+		d1.a=10;
+		d1.display();
+	}
+}
